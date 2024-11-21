277ad774bd7bbf93a4f64fdad7367a8cd1481324
Manticore <vsiva5110@gmail.com.com>
2024-11-21 15:36:59 +0530

    create assignment8

---
+++ main.c
@@ -0,0 +1,21 @@
+/*
++---------------------------------------------------------------------------------------------------------------+
+|                                                                                                               |
+|   Author name : Siva prakasam V                                                                               |
+|   Date        : 20/11/24                                                                                      |
+|                                                                                                               |
+|                                                                                                               |
+|   Question                                                                                                    |
+|   ========                                                                                                    |
+| 														| 
+|     Using `fcntl()` to Get and Set File Descriptor Flags							|
+|   - Write a program that:											|
+|     1. Opens a file  data.txt.										|
+|     2. Uses `fcntl()` to get the current flags set on the file descriptor.					|
+|     3. Changes the file descriptor flags to non-blocking (`O_NONBLOCK`), 					|
+|         then tries to read the file to demonstrate non-blocking behavior.					|
+|                                                                                                               |
++---------------------------------------------------------------------------------------------------------------+
+
+*/
+
